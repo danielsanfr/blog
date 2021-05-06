0e5f16f3a65852fd1b8217e9d796af166a754077
danielsanfr <danielsanfr@users.noreply.github.com>
2021-05-06 03:59:38 +0000

    jekyll build from Action 9b38ee97d941f25d91410b81395325c781c652f1

---
+++ assets/search.js
@@ -0,0 +1,1 @@
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Bem vindo",'url':"/2018/07/01/welcome.html"},{'title':"Mussum Ipsum",'url':"/2020/07/16/test.html"}]};
---
+++ en/assets/search.js
@@ -0,0 +1,1 @@
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/en/2018/07/01/welcome.html"},{'title':"Lorem ipsum",'url':"/en/2020/07/16/test.html"}]};
